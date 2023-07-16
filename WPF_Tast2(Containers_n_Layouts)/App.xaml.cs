66659a992ab8a4d11029dbc8f102068ac2a0338d
vladp_d3fqm45 <vladp_d3fqm45@DESKTOP-FQLK9B1>
2023-07-16 17:08:57 +0200

    Add project files.

---
+++ WPF_Tast2(Containers_n_Layouts)/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_Tast2_Containers_n_Layouts_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ WPF_Tast2(Containers_n_Layouts)/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Tast2_Containers_n_Layouts_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Result.Text += button.Content.ToString();
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Result.Text += button.Content.ToString();
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Result.Text += button.Content.ToString();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Result.Text += button.Content.ToString();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Result.Text += button.Content.ToString();
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Result.Text += button.Content.ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Result.Text += button.Content.ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Result.Text += button.Content.ToString();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Result.Text += button.Content.ToString();
+        }
+
+        private void Button_Click_0(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Result.Text += button.Content.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Text = Result.Text.Remove(Result.Text.Length - 1);
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Result.Text = button.Content.ToString();
+        }
+    }
+}
